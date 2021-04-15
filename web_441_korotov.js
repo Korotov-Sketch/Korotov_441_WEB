a29bcbb405f29d083f7d2d8be953de9638b78b8b
Korotov-Sketch <olegkorotof@mail.ru>
2021-04-15 14:12:56 +0400

    GetAllWorkNow!

--- web_441_korotov.js
+++ web_441_korotov.js
@@ -1,4 +1,4 @@
-class CustomCollection {
+class Storage {
 
     constructor() {
         this._map = new Map()
@@ -91,7 +91,7 @@ class CustomCollection {
         });
     }
 }
-const collection = new CustomCollection();
+const storage = new Storage();
 
 class ValueType1 {
     constructor(value1, value2) {
@@ -108,48 +108,48 @@ class ValueType2 {
     }
 }
 
-let id1 = collection.add(String("stringElem1"));
-let id2 = collection.add(new ValueType1("SecondVal1", "SecondVal2"));
-let id3 = collection.add(new ValueType1("ThirdVal1", "ThirdVal2"));
-let id4 = collection.add(new ValueType2("Fourthvalue1", "FourthVal2", "FourthVal3"));
-let id5 = collection.add(new ValueType2("FifthVal1", "FifthVal2", "FifthVal3"));
+let id1 = storage.add(String("stringElem1"));
+let id2 = storage.add(new ValueType1("SecondVal1", "SecondVal2"));
+let id3 = storage.add(new ValueType1("ThirdVal1", "ThirdVal2"));
+let id4 = storage.add(new ValueType2("FourthVal1", "FourthVal2", "FourthVal3"));
+let id5 = storage.add(new ValueType2("FifthVal1", "FifthVal2", "FifthVal3"));
 
 console.log("Get element by id")
-let elementById = collection.getById(id1);
+let elementById = storage.getById(id1);
 console.log("Item: { id:" + id1 + ", value: " + JSON.stringify(elementById) + "}")
 
-let elementById2 = collection.getById(id2);
+let elementById2 = storage.getById(id2);
 console.log("Item: { id:" + id2 + ", value: " + JSON.stringify(elementById2) + "}")
 
-let elementById3 = collection.getById(id3);
+let elementById3 = storage.getById(id3);
 console.log("Item: { id:" + id3 + ", value: " + JSON.stringify(elementById3) + "}") //без JSON.stringify возвращает типы эл-ов
 
 
-elementById4 = collection.getById(id4);
+elementById4 = storage.getById(id4);
 console.log("Item: { id:" + id4 + ", value: " + JSON.stringify(elementById4) + "}")
 
 console.log("Delete element by id: ")
-console.log("All elements: ", collection.getAll())
-let deleteById = collection.deleteById(id3);
+console.log("All elements: ", storage.getAll())
+let deleteById = storage.deleteById(id3);
 console.log(deleteById)
-console.log("All elements: ", collection.getAll())
-collection.getById(id3) // error
-deleteById = collection.deleteById(id3);
+console.log("All elements: ", storage.getAll())
+storage.getById(id3) // error
+deleteById = storage.deleteById(id3);
 console.log(deleteById)
-console.log("All elements: ", collection.getAll())
+console.log("All elements: ", storage.getAll())
 
 console.log("Replace by id: ")
-let replaceById = collection.replaceById(id3, String("ERROR"))
+let replaceById = storage.replaceById(id3, String("ERROR"))
 console.log(replaceById)
-replaceById = collection.replaceById(id5, String("SUCCESS"));
+replaceById = storage.replaceById(id5, String("SUCCESS"));
 console.log(replaceById)
-console.log("All elements: ", collection.getAll())
+console.log("All elements: ", storage.getAll())
 
 console.log("Update by id: ")
-let updateById = collection.updateById(id2, new ValueType2("field1", "field2", "field3"));
+let updateById = storage.updateById(id2, new ValueType2("field1", "field2", "field3"));
 console.log(updateById)
-updateById = collection.updateById(id3, new ValueType1("newField1!", "newField2!"));
+updateById = storage.updateById(id3, new ValueType1("newField1!", "newField2!"));
 console.log(updateById)
-updateById = collection.updateById(id2, new ValueType1("newField1", "newField2"));
+updateById = storage.updateById(id2, new ValueType1("newField1", "newField2"));
 console.log(updateById)
-console.log("All elements: ", collection.getAll())
+console.log("All elements: ", storage.getAll())
